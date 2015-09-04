e182057cf0e2b7ce71ca02c05ef0ff45d7856108
Aaron Probus <pair+aaron@fundingcircle.com>
2015-09-04 13:39:14 -0700

    Adds license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject do-make-rethink "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "BSD 3-clause"
+            :url "http://opensource.org/licenses/BSD-3-Clause"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.taoensso/timbre "4.1.1"]
                  [rethinkdb "0.10.1"]])
