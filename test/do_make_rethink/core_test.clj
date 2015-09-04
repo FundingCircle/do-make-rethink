4f1efc97db336cc4ce7885c5aed41ed1d29030c3
Aaron Probus <pair+aaron@fundingcircle.com>
2015-09-04 13:32:37 -0700

    Initial commit

---
+++ test/do_make_rethink/core_test.clj
@@ -0,0 +1,7 @@
+(ns do-make-rethink.core-test
+  (:require [clojure.test :refer :all]
+            [do-make-rethink.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
